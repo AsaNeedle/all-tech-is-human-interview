49b255f07d0c0f8f8546f4579dd5218c9ea09e2e
Asa Needle <asa@asaneedle.com>
2024-03-08 09:35:51 -0500

    Find unique candidates

--- frontend/src/components/OrgMatches.tsx
+++ frontend/src/components/OrgMatches.tsx
@@ -1,58 +1,76 @@
 import { useState, useCallback } from 'react'
 
 type Match = {
-    "userId": number;
-    "firstName": string;
-    "lastName": string;
-    "email": string;
-    "userImage": string;
     "matchLevel": number;
     "userInterest": string;
     "matchingRole": string;
 }
 
+type Candidate = {
+    "firstName": string;
+    "lastName": string;
+    "email": string;
+    "userImage": string;
+    "matches": Match[];
+}
+
+type CandidateRecord = Record<string, Candidate>;
+
 const OrgMatches: React.FC = () => {
+    const [hasSearched, setHasSearched] = useState(false);
     const [org, setOrg] = useState('')
-    const [matches, setMatches] = useState<Match[]>([])
+    const [candidateRecord, setCandidateRecord] = useState<CandidateRecord>({})
 
     const onClick = useCallback(() => {
-            const query = `http://127.0.0.1:8000/matches_by_org?org_name=${org}`
+            const query = `${process.env.REACT_APP_DB_URL}matches_by_org?org_name=${org}`
             console.log(query)
             fetch(query, {
                 method: "GET",
             })
             .then((response) => response.json())
-            .then((data) => {
-                console.log(data)
-                // todo: use zod to validate the data we get from the backend
-                setMatches([])
-                setMatches(data.matches.map((match: any) => {
-                    return {
-                        userId: match.user_id,
-                        firstName: match.first_name,
-                        lastName: match.last_name,
-                        email: match.email,
-                        userImage: match.user_image,
+            .then(({ matches }) => {
+                console.log(matches)
+                setCandidateRecord({})
+                const newCandidateRecord: CandidateRecord = {};
+                matches.forEach((match: any) => {
+                    const matchData = {
                         matchLevel: match.match_level,
                         userInterest: match.user_interest,
                         matchingRole: match.matching_role,
                     }
-                }))
+                    if (newCandidateRecord[match.user_id]){
+                        newCandidateRecord[match.user_id].matches.push(matchData)
+                    } else {
+                        newCandidateRecord[match.user_id] = {
+                            firstName: match.first_name,
+                            lastName: match.last_name,
+                            email: match.email,
+                            userImage: match.user_image,
+                            matches: [matchData]
+                        }
+                    }
+                })
+                setCandidateRecord(newCandidateRecord)
+                setHasSearched(true)
             })
             .catch((err) => {
-                setMatches([])
+                setCandidateRecord({})
                 console.log(err.message);
+                setHasSearched(true)
             });
     }, [org])
 
     return (
         <div>
-            <h3>User Search</h3>
+            <h3>Candidate Search</h3>
             <input  value={org} onChange={e => setOrg(e.target.value)} />
             <button onClick={onClick}>Find Matches</button>
-            <div>{(matches.length === 0) ? <div>No Results Found</div> :
-                    matches.map((match) => {
-                        return <div>{`${match.firstName} ${match.lastName}`}</div>
+            <div>{(Object.keys(candidateRecord).length === 0 && hasSearched) ? <div> No Results Found </div> :
+                    Object.entries(candidateRecord).map(([userId, candidate]) => {
+                        return <div key={userId}>
+                                {`${candidate.firstName} ${candidate.lastName}`}
+                                {candidate.matches.map((match, i) => <li key={i}>{match.matchingRole}, score: {match.matchLevel}</li>)}
+                            </div>
                     })
                 }
             </div>
